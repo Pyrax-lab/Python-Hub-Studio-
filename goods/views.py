b0aef7c9970b32542addc0cbfd131c99873eb1e5
Artur <proxaker1111@gmail.com>
2025-01-19 23:30:05 +0200

    slug converter for categories

--- goods/views.py
+++ goods/views.py
@@ -1,73 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .models import Products
 # Create your views here.
 
 
-def catalog(request):
+def catalog(request, catalog_slug):
 
-    # context = {"goods": [
-    #     {'image': 'deps/images/goods/set of tea table and three chairs.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': },
-
-    #      {'image': 'deps/images/goods/set of tea table and two chairs.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 93.00},
-
-    #      {'image': 'deps/images/goods/double bed.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 670.00},
-
-    #      {'image': 'deps/images/goods/kitchen table.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 365.00},
-
-    #      {'image': 'deps/images/goods/kitchen table 2.jpg',
-    #      'name': '    ',
-    #      'description': '',
-    #      'price': 430.00},
-
-    #      {'image': 'deps/images/goods/corner sofa.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 610.00},
-
-    #      {'image': 'deps/images/goods/bedside table.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 55.00},
-
-    #      {'image': 'deps/images/goods/sofa.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 190.00},
-
-    #      {'image': 'deps/images/goods/office chair.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 30.00},
-
-    #      {'image': 'deps/images/goods/plants.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 10.00},
-
-    #      {'image': 'deps/images/goods/flower.jpg',
-    #      'name': '',
-    #      'description': '',
-    #      'price': 15.00},
-
-    #      {'image': 'deps/images/goods/strange table.jpg',
-    #      'name': '    ',
-    #      'description': '',
-    #      'price': 25.00},
-    #     ]}
-
-    goods = Products.objects.all()
+    if catalog_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug = catalog_slug))
 
     return render(request, "goods/catalog.html", {"goods": goods})
 
