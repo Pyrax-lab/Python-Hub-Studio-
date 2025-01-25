0f8679428d9d00684dc7348c13d39c21be08fa16
Artur <proxaker1111@gmail.com>
2025-01-25 18:20:42 +0200

    the product filter on the site page and the urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,5 +1,5 @@
 from goods.models import Categories 
-
+from django.utils.http import urlencode
 from django import template 
 
 register = template.Library()
@@ -7,3 +7,10 @@ register = template.Library()
 @register.simple_tag()
 def goods_category():
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)  # позволяет использовать переменную context тут
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,45 @@
 from django.shortcuts import render, get_list_or_404
 from .models import Products
+from django.core.paginator import Paginator
+from django.utils.http import urlencode
 # Create your views here.
 
 
 def catalog(request, catalog_slug):
+
+
+    order_by1 = request.GET.get("order_by", None)
+    on_sale = request.GET.get("on_sale", None)
+    current_slug = catalog_slug
+
+    print(order_by1)
+    print(on_sale)
 
     if catalog_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug = catalog_slug))
 
-    return render(request, "goods/catalog.html", {"goods": goods})
+    if order_by1 and order_by1 != "default":
+        print("11")
+        goods = goods.order_by(order_by1)
+    if on_sale:
+        
+        goods = goods.filter(price__lt=200)
+
+    # get_page = 1
+    # if request.method == "GET":
+    #     if request.GET.get('page'):
+    #         get_page = request.GET.get('page', 1)
+
+    get_page = request.GET.get('page', 1)
+
+    get_request = urlencode(request.GET.dict())
+    print(get_request)
+    paginator = Paginator(goods, 3)
+    curent_page = paginator.page(get_page)
+
+    return render(request, "goods/catalog.html", {"goods": curent_page, "current_slug": current_slug, "get_request": get_request})
 
     
 
