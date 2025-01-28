1614a676b1e10c74ec4a2639fb42ed2ff111b8f0
Artur <proxaker1111@gmail.com>
2025-01-28 21:40:41 +0200

    Add serach with id, name, description

--- goods/urls.py
+++ goods/urls.py
@@ -5,6 +5,7 @@ app_name = "goods"
 
 
 urlpatterns = [
+    path("serch", views.catalog, name="search"),
     path("<slug:catalog_slug>", views.catalog, name="index"),
     path("product/<slug:product_slug>/", views.product, name="product")
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,22 +1,35 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Products
 from django.core.paginator import Paginator
 from django.utils.http import urlencode
+from django.db.models import Q
 # Create your views here.
 
 
-def catalog(request, catalog_slug):
+def catalog(request, catalog_slug=None):
 
 
     order_by1 = request.GET.get("order_by", None)
     on_sale = request.GET.get("on_sale", None)
+    query = request.GET.get("q", None)
     current_slug = catalog_slug
-
-    print(order_by1)
-    print(on_sale)
+    print(query)
+    # print(order_by1)
+    # print(on_sale)
 
     if catalog_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        if query.isdigit() and len(query) <= 5:
+            goods = Products.objects.filter(id = int(query))
+        else:
+            words = [word for word in query.split() if len(word) > 2 ]
+            q_query = Q()
+
+            for word in words:
+                q_query |= Q(description__icontains = word) | Q(name__icontains = word)
+            goods = Products.objects.filter(q_query)
+
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug = catalog_slug))
 
