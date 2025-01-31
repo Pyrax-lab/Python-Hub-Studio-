8593e2fc9faa3f3076fc8e489d2e78988b30aee9
Artur <proxaker1111@gmail.com>
2025-01-31 23:24:50 +0200

    added abstract user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser 
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    image = models.ImageField(upload_to="image_users", blank = True, null = True)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
